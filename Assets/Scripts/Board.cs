07d2f39632e939505227266f14ff11aa250116bd
pesduo <pesduo05@gmail.com>
2025-03-15 22:51:50 +0700

    match3test

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using UnityEngine;
 public sealed class Board : MonoBehaviour
 {
     public static Board Instance { get; private set; }
+    [SerializeField] private AudioSource _source;
     public Row[] rows;
     public Tile[,] tiles { get; private set; }
     public int width => tiles.GetLength(0); 
@@ -28,10 +30,15 @@ public sealed class Board : MonoBehaviour
                 var tile = rows[y].tiles[x];
                 tile.x = x;
                 tile.y = y;
-                tile.item = itemDatabase.Items[Random.Range(0, itemDatabase.Items.Length)];
+                tile.item = itemDatabase.Items[UnityEngine.Random.Range(0, itemDatabase.Items.Length)];
                 tiles[x,y] = tile;
             }
         }
+    }
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.A)) return;
+        foreach (var connectedTile in tiles[0, 0].GetConnectedTiles()) connectedTile.icon.transform.DOScale(1.25f, TweenDuration).Play(); 
     }
     public async void Select(Tile tile)
     {
@@ -39,6 +46,16 @@ public sealed class Board : MonoBehaviour
         if (_selection.Count < 2) return;
         Debug.Log($"Selected tiles at({_selection[0].x},{_selection[0].y} and {_selection[1].x},{_selection[1].y})");
         await Swap(_selection[0], _selection[1]);
+
+        if (CanPop())
+        {
+             Pop();
+        }
+        else
+        {
+            await Swap(_selection[0], _selection[1]);
+        }
+        _selection.Clear();
     }
     public async Task Swap(Tile tile1, Tile tile2)
     {
@@ -64,7 +81,47 @@ public sealed class Board : MonoBehaviour
         var tileItem = tile1.item;
 
         tile1.item = tile2.item;
-        tile2.item = tile1.item;
+        tile2.item = tileItem;
     }
+    private bool CanPop()
+    {
+        for (var y = 0; y < height; y++)
+        {
+            for (var x = 0; x < width; x++)
+            {
+                if (tiles[x, y].GetConnectedTiles().Skip(1).Count() >=2 )
+                    return true;
+            }
+        }
+        return false;   
+    }
+    private async void Pop()
+    {
+        for (var y = 0; y < height; y++)
+        {
+            for (var x = 0; x < width; x++)
+            {
+                var tile = tiles[x, y];
+                var connectedTiles = tile.GetConnectedTiles();
+                if (connectedTiles.Skip(1).Count() < 2) continue;
 
+                var deflateSequence = DOTween.Sequence();
+                foreach(var connectedTile in connectedTiles)
+                {
+                    deflateSequence.Join(connectedTile.icon.transform.DOScale(Vector3.zero, TweenDuration));
+                }
+                await deflateSequence.Play().AsyncWaitForCompletion();
+                ScoreCounter.Instance.Score+= tile.item.value *connectedTiles.Count;
+                var inflateSequence = DOTween.Sequence();
+                foreach(var connectedTile in connectedTiles)
+                {
+                    connectedTile.item = itemDatabase.Items[UnityEngine.Random.Range(0, itemDatabase.Items.Length)];
+                    inflateSequence.Join(connectedTile.icon.transform.DOScale(Vector3.one, TweenDuration));
+                }
+                await inflateSequence.Play().AsyncWaitForCompletion();
+                x = 0;
+                y = 0;
+            }
+        }
+    }    
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,8 +21,37 @@ public class Tile : MonoBehaviour
     }
     public Image icon;
     public Button button;
+    public Tile Left => x > 0 ? Board.Instance.tiles[x - 1, y] : null;
+    public Tile Top => y > 0 ? Board.Instance.tiles[x, y - 1] : null;
+    public Tile Right => x < Board.Instance.width - 1 ? Board.Instance.tiles[x + 1, y] : null;
+    public Tile Bottom => y < Board.Instance.height - 1 ? Board.Instance.tiles[x, y + 1] : null;
+
+    public Tile[] Neighbours => new[]
+    {
+        Left, Top, Right, Bottom
+    };
     private void Start()
     {
-        button.onClick.AddListener(()=>Board.Instance.Select(this));
+        button.onClick.AddListener(() => Board.Instance.Select(this));
+    }
+    public List<Tile> GetConnectedTiles(List<Tile> exclude = null)
+    {
+        var result = new List<Tile> { this, };
+        if (exclude == null)
+        {
+            exclude = new List<Tile> { this, };
+        }
+        else
+        {
+            exclude.Add(this);
+        }
+        
+        foreach (var neighbour in Neighbours)
+        {
+            if(neighbour == null || exclude.Contains(neighbour) || neighbour.item != item) continue;
+            result.AddRange(neighbour.GetConnectedTiles(exclude));
+        }
+
+        return result;
     }
 }
